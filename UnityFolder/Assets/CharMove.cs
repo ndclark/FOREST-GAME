46ab72da4f2237f43de75ec66ced386c58c71984
ndclark <ndclark@mail.bradley.edu>
2014-09-12 22:26:12 -0500

    Move Script

--- UnityFolder/Assets/CharMove.cs
+++ UnityFolder/Assets/CharMove.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class CharMove : MonoBehaviour {
-
+	public float vertSpeed = 0.0f;
+	public float horzSpeed = 0.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +12,21 @@ public class CharMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void FixedUpdate () 
+	{
+		Vector2 targetDirection = Vector2.zero;
+		if (Input.GetKey (KeyCode.A))
+			targetDirection += -Vector2.right*horzSpeed;
+		else if (Input.GetKey (KeyCode.D))
+			targetDirection += Vector2.right*horzSpeed;
+		else if (Input.GetKey (KeyCode.W))
+			targetDirection += Vector2.up *vertSpeed;
+		else if (Input.GetKey (KeyCode.S))
+			targetDirection += -Vector2.up * vertSpeed;
+	
+
+		rigidbody2D.velocity = targetDirection * Time.deltaTime;
 	}
 }
