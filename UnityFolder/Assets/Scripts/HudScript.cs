579e3eab18639b1096ca8d3e8b5bbdb95f677ec0
ccwatson <ccwatson@mail.bradley.edu>
2014-09-13 09:26:38 -0500

    update

--- UnityFolder/Assets/Scripts/HudScript.cs
+++ UnityFolder/Assets/Scripts/HudScript.cs
@@ -4,12 +4,30 @@ using System.Collections;
 public class HudScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	 float Player_Health = 100f;
+	 float Player_Water = 100f;
+
+	public float Health_Fall;
+
+	public GUIStyle Health_Bar_GUI;
+	public GUIStyle Water_Bar_GUI;
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		Player_Water = Player_Water - Health_Fall;
+
+		if (Player_Water <= 0)
+		{
+			Player_Water = 0;
+			Player_Health = Player_Health - Health_Fall;
+		}
 	
+	}
+
+	void OnGUI()
+	{
+		GUI.Box (new Rect (5, 5, Screen.width / 3/ ( 100 / Player_Health) , 20), "" + (int)Player_Health, Health_Bar_GUI);
+		GUI.Box (new Rect (5, 30, Screen.width / 3/ ( 100 / Player_Water) , 20), "" + (int)Player_Water, Water_Bar_GUI);
 	}
 }
