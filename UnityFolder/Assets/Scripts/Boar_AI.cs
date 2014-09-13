3dd460529a73b8dd632c159c31803145f70d4a9f
jestill2018 <jestill@mail.bradley.edu>
2014-09-13 16:15:41 -0500

    Boar AI Script
    Idle pose in the forward is still incorrect

---
+++ UnityFolder/Assets/Scripts/Boar_AI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boar_Ai : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		transform.LookAt(Vector3.one);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
